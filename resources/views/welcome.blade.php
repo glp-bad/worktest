90280ae36c18d4a59723772961ae5c3a1edc5054
GLP <gavrilapaul@gmail.com>
2021-11-16 19:32:19 +0200

    windows: public folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,19 @@
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <link rel="icon" href="/favicon.ico">
         <title>vue-client</title>
-        <link href="/js/about.js" rel="prefetch">
-        <link href="/css/app.css" rel="preload" as="style">
-        <link href="/js/app.js" rel="preload" as="script">
-        <link href="/js/chunk-vendors.js" rel="preload" as="script">
-        <link href="/css/app.css" rel="stylesheet">
+        <link href="{{ asset(mix('css/app.css')) }}" type="text/css" rel="stylesheet" />
+        <meta name="csrf-token" value="{{ csrf_token() }}" />
+
     </head>
     <body>
         <noscript><strong>We're sorry but vue-client doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
             <div id="app"></div>
-            <script src="/js/chunk-vendors.js"></script>
-            <script src="/js/app.js"></script>
+
+            <script src="{{ asset(mix('js/about.js')) }}" type="text/javascript"></script>
+            <script src="{{ asset(mix('js/chunk-vendors.js')) }}" type="text/javascript"></script>
+            <script src="{{ asset(mix('js/app.js')) }}" type="text/javascript"></script>
+
+
+
     </body>
 </html>
