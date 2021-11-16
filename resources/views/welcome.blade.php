3db81104f4ad4f2791edf7e86aca2437846e0d95
GLP <gavrilapaul@gmail.com>
2021-11-16 19:36:02 +0200

    windows: public folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link rel="icon" href="/favicon.ico">
+        <link rel="icon" href="img/favicon.ico">
         <title>vue-client</title>
         <link href="{{ asset(mix('css/app.css')) }}" type="text/css" rel="stylesheet" />
         <meta name="csrf-token" value="{{ csrf_token() }}" />
