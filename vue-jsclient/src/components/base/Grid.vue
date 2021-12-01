55dac47134d6b6a42e18c2f3c2cc962d96b453a3
GLP <gavrilapaul@gmail.com>
2021-12-01 17:23:16 +0200

    windows: grid

--- vue-jsclient/src/components/base/Grid.vue
+++ vue-jsclient/src/components/base/Grid.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ff-grid">
+    <div class="ff-grid" :ref=this.REF_DIV_TABLE>
         <table class="ff-table">
             <thead class="ff-thead" :ref=this.REF_THEAD>
                 <tr>
@@ -25,10 +25,12 @@
 	export default {
 		name: "grid",
 		created() {
+            this.REF_DIV_TABLE      = 'refDivTable',
 			this.REF_THEAD      = 'refThead'
 		},
 		props: {
-		    pHeader: {type: Array, required: true}
+		    pHeader: {type: Array, required: true},
+            pHeight: {type: Number, default: 300, required: false}
 		},
 		mounted() {
 		    this.getDataFromServer();
@@ -36,16 +38,26 @@
 		},
         methods:{
             cfgGrid: function () {
+                let divTable = this.$refs[this.REF_DIV_TABLE];
+
                 let headerCells = this.$refs[this.REF_THEAD].rows[0].cells;
                 this.cfgGridHeader(headerCells);
+
+                // form, ordinea conteaza pentru: this.engine.widthGridFromCell
+                divTable.style.width = this.engine.widthGridFromCell  + 'px';
+                divTable.style.height = this.pHeight  + 'px';
             },
             cfgGridHeader: function (headerCells) {
                 for (let i = 0; i < headerCells.length; i++) {
                     let width = this.$vanilla.getAtributeValueFromArrayObject(this.pHeader,'id',headerCells[i].getAttribute('id'),'width');
-                    //headerCells[i].style.width = w + 'px';
-                    //headerCells[i].style.fixedWidth = w + 'px';
-                    headerCells[i].firstChild.style.width = width + 'px';
-                    headerCells[i].firstChild.style.fixedWidth = width + 'px';
+
+                    this.engine.widthGridFromCell = this.engine.widthGridFromCell + width;
+
+                    headerCells[i].style.width = width + 'px';
+                    headerCells[i].style.fixedWidth = width + 'px';
+                    //headerCells[i].firstChild.style.width = width + 'px';
+                    //headerCells[i].firstChild.style.fixedWidth = width + 'px';
+
                 }
             },
             getDataFromServer: function () {
@@ -54,7 +66,7 @@
                 dataTest.push({name: 'Vasile',  act: 'se duce la piata si face cumparaturii 004', rez: 'nu a castigat nimic 004', var: 'variaza + 4'});
                 dataTest.push({name: 'Ion',     act: 'se duce la piata si face cumparaturii 005', rez: 'nu a castigat nimic 005', var: 'variaza + 5'});
 
-                for(let i=0; i<5; i++){
+                for(let i=0; i<10; i++){
                     dataTest.push({name: i+' Vasile',  act: 'se duce la piata si face cumparaturii 00' + i, rez: 'nu a castigat nimic ' +i, var: 'variaza +' + i});
                     dataTest.push({name: i+' Ion',  act: 'se duce la piata si face cumparaturii 00' + i, rez: 'nu a castigat nimic ' +i, var: 'variaza +' + i});
                 }
@@ -68,6 +80,7 @@
 		data () {
 			return {
 				engine:{
+				    widthGridFromCell: 0
                 },
                 rezultData: new Array()
             }
